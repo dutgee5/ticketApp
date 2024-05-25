e3390b65e9b5b94cbd4c190ce3455ce73369ffc2
theDone <done.muzaffer5@gmail.com>
2024-05-25 23:58:04 +0300

    buttom tabs created

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -1,24 +1,77 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { Example, Startup } from '@/screens';
-import { useTheme } from '@/theme';
+import { useTheme } from "@/theme";
 
-import type { ApplicationStackParamList } from '@/types/navigation';
+import type { ApplicationStackParamList } from "@/types/navigation";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "@/screens/HomeScreen";
+import SearchScreen from "@/screens/SearchScreen";
+import TicketScreen from "@/screens/TicketScreen";
+import UserScreen from "@/screens/UserScreen";
+
+import HomeActiveIcon from "@/theme/icons/HomeActiveIcon";
+import HomePassive from "@/theme/icons/HomePassiveIcon";
+import SearchActive from "@/theme/icons/SearchActiveIcon";
+import SearchPassive from "@/theme/icons/SearchPassiveIcon";
+import TicketActiveIcon from "@/theme/icons/TicketActiveIcon";
+import TicketPassive from "@/theme/icons/TicketPassiveIcon";
+import TicketPassiveIcon from "@/theme/icons/TicketPassiveIcon";
+import UserActiveIcon from "@/theme/icons/UserActiveIcon";
+import UserPassiveIcon from "@/theme/icons/UserPassiveIcon";
 
 const Stack = createStackNavigator<ApplicationStackParamList>();
+const Tab = createBottomTabNavigator();
 
 function ApplicationNavigator() {
-	const { variant, navigationTheme } = useTheme();
+  const { variant, navigationTheme } = useTheme();
 
-	return (
-		<NavigationContainer theme={navigationTheme}>
-			<Stack.Navigator key={variant} screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="Startup" component={Startup} />
-				<Stack.Screen name="Example" component={Example} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerTitleAlign: "center",
+            tabBarIcon: ({ focused }) => {
+              return focused ? <HomeActiveIcon /> : <HomePassive />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => {
+              return focused ? <SearchActive /> : < SearchPassive/>;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Tickets"
+          component={TicketScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => {
+              return focused ? <TicketActiveIcon /> : <TicketPassiveIcon />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => {
+              return focused ? <UserActiveIcon /> : <UserPassiveIcon />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default ApplicationNavigator;
